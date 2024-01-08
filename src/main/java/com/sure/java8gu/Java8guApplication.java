4ee120f9d983d826f4f31d68d06fcc9437dcb4fb
Sure <wangfengshuo2012@163.com>
2024-01-08 16:12:32 +0800

    init project frame

---
+++ src/main/java/com/sure/java8gu/Java8guApplication.java
@@ -0,0 +1,13 @@
+package com.sure.java8gu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8guApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8guApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sure/java8gu/Java8guApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sure.java8gu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8guApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
